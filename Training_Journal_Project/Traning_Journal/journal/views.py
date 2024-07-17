1afe88311a3cc666f863f0cd33d37fcac2c23250
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2024-07-17 22:03:38 +0530

    Created the template for login and signup page

--- Training_Journal_Project/Traning_Journal/journal/views.py
+++ Training_Journal_Project/Traning_Journal/journal/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Traning Journal Test")
+    return render(request, "journal/index.html")
+
+def login(request):
+    return render(request, "journal/login.html")
+
+def signup(request):
+    return render(request, "journal/signup.html")
